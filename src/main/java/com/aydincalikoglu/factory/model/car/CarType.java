432d676ee3aefe55d967526244861c8659d52950
acalikoglu <aydin.calikoglu@cimri.com>
2021-01-10 20:51:49 +0300

    init

--- src/main/java/com/aydincalikoglu/factory/model/car/CarType.java
+++ src/main/java/com/aydincalikoglu/factory/model/car/CarType.java
@@ -3,16 +3,11 @@ package com.aydincalikoglu.factory.model.car;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import org.springframework.core.convert.converter.Converter;
 
-public enum CarType implements Converter<String, CarType> {
+public enum CarType {
     CABRIO, HATCHBACK, SEDAN;
 
     @JsonCreator
     public static CarType fromString(String source) {
-        return CarType.valueOf(source.toUpperCase());
-    }
-
-    @Override
-    public CarType convert(String source) {
         return CarType.valueOf(source.toUpperCase());
     }
 }
